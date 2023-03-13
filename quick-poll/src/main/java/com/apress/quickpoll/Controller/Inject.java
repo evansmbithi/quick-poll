b711bd704070b81152580183b1c26fe209d22b5d
evansmbithi <evholmbithy@gmail.com>
2023-03-13 10:15:43 +0000

    Computresult

--- quick-poll/src/main/java/com/apress/quickpoll/Controller/Inject.java
+++
@@ -1,5 +0,0 @@
-package com.apress.quickpoll.Controller;
-
-public @interface Inject {
-
-}
--- quick-poll/src/main/java/com/apress/quickpoll/Controller/PollController.java
+++ quick-poll/src/main/java/com/apress/quickpoll/Controller/PollController.java
@@ -1,14 +1,18 @@
 package com.apress.quickpoll.Controller;
 
 import java.net.URI;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -31,6 +35,11 @@ public class PollController {
         return new ResponseEntity<>(allPolls, HttpStatus.OK);
     }
 
+    /*
+     *  The @RequestBody annotation tells Spring 
+     * that the entire request body needs to be converted
+     * to an instance of Poll.
+     */
     @PostMapping("/polls")
     public ResponseEntity<?> createPoll (@RequestBody Poll poll){
         poll = pollRepository.save(poll);
@@ -44,5 +53,30 @@ public class PollController {
         
         responseHeaders.setLocation(newPollUri);
         return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+    // individual poll
+    @GetMapping("/polls/{pollId}")
+    public ResponseEntity <?> getPoll(@PathVariable Long pollId) throws Exception{
+        Optional<Poll> poll = pollRepository.findById(pollId);
+        if(!poll.isPresent()){
+            throw new Exception("Poll not found");
+        }
+        return new ResponseEntity<>(poll.get(), HttpStatus.OK);                
+    }
+
+    @PutMapping("/polls/{pollId}")
+    public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId){
+        // set id
+        poll.setId(pollId);
+        // save the entity
+        Poll newPoll = pollRepository.save(poll);
+        return new ResponseEntity<>(newPoll,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/polls/{pollId}")
+    public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+        pollRepository.deleteById(pollId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
